1febce689d72b65623cc0c38e596fe586ac59f7c
wyz19980928 <50905507+wyz19980928@users.noreply.github.com>
2023-05-22 20:11:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -16,7 +16,19 @@ const USER_CONFIG = {
   PROVINCE: '江苏',
   CITY: '泰州',
   
-
+🗓️{{date.DATA}}
+城市：{{city.DATA}}
+天气☁️：{{weather.DATA}}
+温度🌡️: {{min_temperature.DATA}}--{{max_temperature.DATA}}
+风向：{{wind_direction.DATA}}
+风级：{{wind_scale.DATA}}
+温馨提示：{{notice.DATA}}
+值得纪念：{{birthday_message.DATA}}
+💗：{{earthy_love_words.DATA}}
+每日一句😋
+中文：{{note_ch.DATA}}
+English：{{note_en.DATA}}
+和{{poetry_author.DATA}}约个会：{{poetry_content.DATA}}
 
   USERS: [
     {
